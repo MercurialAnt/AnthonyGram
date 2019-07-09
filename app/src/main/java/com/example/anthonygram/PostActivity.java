876429405989229fcc549943ff1c5fc578b2a606
MercurialAnt <mata.anthony98@gmail.com>
2019-07-09 15:58:43 -0700

    I'm able to do a post that includes taking a picture and a description. I know just have to add the home feed.

--- app/src/main/java/com/example/anthonygram/PostActivity.java
+++ app/src/main/java/com/example/anthonygram/PostActivity.java
@@ -1,15 +1,23 @@
 package com.example.anthonygram;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.anthonygram.model.Post;
 import com.parse.FindCallback;
@@ -28,11 +36,13 @@ public class PostActivity extends AppCompatActivity {
     private Button btnCreate;
     private Button btnRefresh;
     private Button btnProfile;
+    private Button btnCamera;
+    private ImageView ivPostImage;
 
-//    public final String APP_TAG = "MyCustomApp";
-//    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
-//    public String photoFileName = "photo.jpg";
-//    File photoFile;
+    public final String APP_TAG = "AnthonyGram";
+    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
+    public String photoFileName = "photo.jpg";
+    private File photoFile;
 
 
     @Override
@@ -55,6 +65,9 @@ public class PostActivity extends AppCompatActivity {
         btnCreate = findViewById(R.id.btnCreate);
         btnRefresh = findViewById(R.id.btnRefresh);
         btnProfile = findViewById(R.id.btnProfile);
+        btnCamera = findViewById(R.id.btnCamera);
+        ivPostImage = findViewById(R.id.ivPostImage);
+
 
         btnProfile.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,6 +76,14 @@ public class PostActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onLaunchCamera(v);
+            }
+        });
+
+
 
         btnCreate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,8 +91,7 @@ public class PostActivity extends AppCompatActivity {
                 final String description = etDescription.getText().toString();
                 final ParseUser user = ParseUser.getCurrentUser();
 
-                final File file = new File("");
-                final ParseFile parseFile = new ParseFile(file);
+                final ParseFile parseFile = new ParseFile(photoFile);
 
                 createPost(description, parseFile, user);
             }
@@ -83,8 +103,63 @@ public class PostActivity extends AppCompatActivity {
             }
         });
 
-        loadTopPosts();
+//        loadTopPosts();
 
+    }
+
+    public void onLaunchCamera(View view) {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference to access to future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(PostActivity.this, "com.codepath.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    // Returns the File for a photo stored on disk given the fileName
+    public File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), APP_TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(APP_TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+        return file;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+
+                ivPostImage.setImageBitmap(takenImage);
+                ivPostImage.setVisibility(View.VISIBLE);
+            } else { // Result was a failure
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     private void createPost(String description, ParseFile imageFile, ParseUser user) {
@@ -98,6 +173,10 @@ public class PostActivity extends AppCompatActivity {
             public void done(ParseException e) {
                 if (e == null) {
                     Log.d("Home Activity", "Create post success");
+                    etDescription.setText("");
+                    ivPostImage.setVisibility(View.INVISIBLE);
+                    Intent intent = new Intent(PostActivity.this, HomeActivity.class);
+                    startActivity(intent);
                 } else {
                     e.printStackTrace();
                 }
